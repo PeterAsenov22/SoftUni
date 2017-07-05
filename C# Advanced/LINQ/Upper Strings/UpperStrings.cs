fd2bf98c4433b07ad9c26dac5114882de9e839f6
Peter Asenov <peter12345@abv.bg>
2017-07-05 14:37:48 +0300

    Add files via upload

---
+++ C# Advanced/LINQ/Upper Strings/UpperStrings.cs
@@ -0,0 +1,15 @@
+﻿namespace Upper_Strings
+{
+    using System;
+    using System.Linq;
+
+    public class UpperStrings
+    {
+        public static void Main()
+        {
+            var names = Console.ReadLine().Split(' ').ToList(); ;
+            names.ForEach(x=>Console.Write(x.ToUpper()+ " "));
+            Console.WriteLine();
+        }
+    }
+}
