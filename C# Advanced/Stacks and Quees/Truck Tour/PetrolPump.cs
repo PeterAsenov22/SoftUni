967d55bfb334a5843ff58c2e73ece2fe8997a2d1
Peter Asenov <peter12345@abv.bg>
2017-07-05 14:49:15 +0300

    Add files via upload

---
+++ C# Advanced/Stacks and Quees/Truck Tour/PetrolPump.cs
@@ -0,0 +1,9 @@
+﻿namespace Truck_Tour
+{
+    public class PetrolPump
+    {
+        public int PumpIndex { get; set; }
+        public long Fuel { get; set; }
+        public long KmToNextStation { get; set; }
+    }
+}
---
+++ C# Advanced/Stacks and Quees/Truck Tour/TruckTour.cs
@@ -0,0 +1,61 @@
+﻿namespace Truck_Tour
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class TruckTour
+    {
+        public static void Main()
+        {
+            long n = long.Parse(Console.ReadLine());
+            var pumps = new Queue<PetrolPump>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var inputParams = Console.ReadLine().Split(new[] {' '},StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+                var pump = new PetrolPump
+                {
+                    Fuel = inputParams[0],
+                    KmToNextStation = inputParams[1],
+                    PumpIndex = i
+                };
+
+                pumps.Enqueue(pump);               
+            }
+
+            PetrolPump starterPump = null;
+            var completeJourney = false;
+
+            while (true)
+            {
+                var currentPump = pumps.Dequeue();
+                pumps.Enqueue(currentPump);
+                starterPump = currentPump;
+                var petrolInTank = currentPump.Fuel;
+
+                while (petrolInTank>=currentPump.KmToNextStation)
+                {
+                    petrolInTank -= currentPump.KmToNextStation;
+
+                    currentPump = pumps.Dequeue();
+                    pumps.Enqueue(currentPump);
+
+                    if(currentPump == starterPump)
+                    {
+                        completeJourney = true;
+                        break;
+                    }
+
+                    petrolInTank += currentPump.Fuel;
+                }
+
+                if(completeJourney)
+                {
+                    Console.WriteLine(starterPump.PumpIndex);
+                    break;
+                }
+            }           
+        }
+    }
+}
