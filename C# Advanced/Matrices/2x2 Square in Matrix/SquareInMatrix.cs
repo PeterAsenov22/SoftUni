63a6f7618e460db8b79ff0b138fb26f91dc3ab8b
Peter Asenov <peter12345@abv.bg>
2017-07-06 23:16:12 +0300

    Add files via upload

---
+++ C# Advanced/Matrices/2x2 Square in Matrix/SquareInMatrix.cs
@@ -0,0 +1,38 @@
+﻿namespace _2x2_Square_in_Matrix
+{
+    using System;
+    using System.Linq;
+
+    public class SquareInMatrix
+    {
+        public static void Main()
+        {
+            var matrixParams = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            var rows = matrixParams[0];
+            var cols = matrixParams[1];
+            var matrix = new char[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                matrix[i] = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse).ToArray();
+            }
+
+            var count = 0;
+            for (int i = 0; i < rows-1; i++)
+            {
+                for (int j = 0; j < cols-1; j++)
+                {
+                    if (matrix[i][j] == matrix[i][j + 1] && matrix[i + 1][j] == matrix[i][j] &&
+                        matrix[i][j] == matrix[i + 1][j + 1])
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
