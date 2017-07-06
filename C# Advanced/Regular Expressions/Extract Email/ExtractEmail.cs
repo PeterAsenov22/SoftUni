b5e04ac1e865eb24cd6952a816aec33d752dd1af
Peter Asenov <peter12345@abv.bg>
2017-07-06 23:10:52 +0300

    Add files via upload

---
+++ C# Advanced/Regular Expressions/Extract Email/ExtractEmail.cs
@@ -0,0 +1,20 @@
+﻿namespace Extract_Email
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class ExtractEmail
+    {
+        public static void Main()
+        {
+            var regex = new Regex(@"\s{1}[A-Za-z0-9][\w\d\.-]*[A-Za-z0-9]\@[A-Za-z][A-Za-z-]*[A-Za-z]\.[A-Za-z][A-Za-z-]*[A-Za-z](\.[a-zA-Z][A-Za-z-]*[A-Za-z]+)*");
+            var input = Console.ReadLine();
+            var matches = regex.Matches(input);
+
+            foreach (var match in matches)
+            {
+                Console.WriteLine(match.ToString().Remove(0,1));
+            }
+        }
+    }
+}
