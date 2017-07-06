cdf3cdf44d46b6dd343cab2897242132ec4a1312
Peter Asenov <peter12345@abv.bg>
2017-07-06 17:51:29 +0300

    Add files via upload

---
+++ C# Advanced/Streams and Files/Copy Binary File/CopyBinaryFile.cs
@@ -0,0 +1,32 @@
+﻿namespace Copy_Binary_File
+{
+    using System.IO;
+
+    public class CopyBinaryFile
+    {
+        private const string DoggoImagePath = "../../doggo.jpg";
+        private const string DestitinationPath = "../../result.jpg";
+
+        public static void Main()
+        {
+            using (var source = new FileStream(DoggoImagePath, FileMode.Open))
+            {
+                using (var destination = new FileStream(DestitinationPath, FileMode.Create))
+                {
+                    var buffer = new byte[4096];
+
+                    while (true)
+                    {
+                        var readBytes = source.Read(buffer, 0, buffer.Length);
+                        if (readBytes == 0)
+                        {
+                            break;
+                        }
+
+                        destination.Write(buffer,0,readBytes);
+                    }
+                }
+            }
+        }
+    }
+}
