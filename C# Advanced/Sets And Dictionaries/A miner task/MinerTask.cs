ff93403b8bf7f34ee028a46dbbcf80beab4755b1
Peter Asenov <peter12345@abv.bg>
2017-07-06 12:53:11 +0300

    Add files via upload

---
+++ C# Advanced/Sets And Dictionaries/A miner task/MinerTask.cs
@@ -0,0 +1,33 @@
+﻿namespace A_miner_task
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class MinerTask
+    {
+        public static void Main()
+        {
+            var dictionary = new Dictionary<string,long>();
+            var material = Console.ReadLine();
+
+            while (material!="stop")
+            {
+                var quantity = long.Parse(Console.ReadLine());
+
+                if (!dictionary.ContainsKey(material))
+                {
+                    dictionary[material] = 0;
+                }
+
+                dictionary[material] += quantity;
+
+                material = Console.ReadLine();
+            }
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
