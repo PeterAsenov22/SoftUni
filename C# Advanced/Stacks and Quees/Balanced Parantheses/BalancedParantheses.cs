416f543d81fad6eaed47cc8e3969245d52feb66f
Peter Asenov <peter12345@abv.bg>
2017-07-07 12:38:09 +0300

    Add files via upload

---
+++ C# Advanced/Stacks and Quees/Balanced Parantheses/BalancedParantheses.cs
@@ -0,0 +1,67 @@
+﻿namespace Balanced_Parantheses
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class BalancedParantheses
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+
+            if (input.Length % 2 != 0)
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                var notBalanced = false;
+                var stack = new Stack<char>();
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (input[i] == '(' || input[i] == '{' || input[i] == '[')
+                    {
+                        stack.Push(input[i]);
+                    }
+                    else
+                    {
+                        if (stack.Count > 0)
+                        {
+                            var curr = stack.Pop();
+                            if (curr == '(' && input[i] != ')')
+                            {
+                                notBalanced = true;
+                                break;
+                            }
+                            if (curr == '{' && input[i] != '}')
+                            {
+                                notBalanced = true;
+                                break;
+                            }
+                            if (curr == '[' && input[i] != ']')
+                            {
+                                notBalanced = true;
+                                break;
+                            }
+
+                        }
+                        else
+                        {
+                            notBalanced = true;
+                            break;
+                        }
+                    }
+                }
+
+                if(notBalanced)
+                {
+                    Console.WriteLine("NO");
+                }
+                else
+                {
+                    Console.WriteLine("YES");
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/Stacks and Quees/Basic Queue Operations/BasicQueueOperations.cs
@@ -0,0 +1,47 @@
+﻿namespace Basic_Queue_Operations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class BasicQueueOperations
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var numbers = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var N = input[0];
+            var S = input[1];
+            var X = input[2];
+
+            var queue = new Queue<int>(numbers);
+
+            for (int i = 0; i < S; i++)
+            {
+                queue.Dequeue();
+
+                if (queue.Count == 0)
+                {
+                    break;
+                }
+            }
+
+            if (queue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                if (queue.Contains(X))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(queue.Min());
+                }
+            }
+        }
+    }
+}
