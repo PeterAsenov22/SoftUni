ced436911df9f580fa302a1d681aab5ac715b36b
Peter Asenov <peter12345@abv.bg>
2017-07-07 12:42:40 +0300

    Add files via upload

---
+++ C# Advanced/Stacks and Quees/Decimal to Binary/DecimalToBinary.cs
@@ -0,0 +1,35 @@
+﻿namespace Decimal_to_Binary
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class DecimalToBinary
+    {
+        public static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            if(number == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                var stack = new Stack<int>();
+
+                while (number!=0)
+                {
+                    stack.Push(number % 2);
+                    number = number / 2;
+                }
+
+                while (stack.Count>0)
+                {
+                    Console.Write(stack.Pop());
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C# Advanced/Stacks and Quees/Hot Potato/HotPotato.cs
@@ -0,0 +1,30 @@
+﻿namespace Hot_Potato
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class HotPotato
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split().ToArray();
+            var queue = new Queue<string>(input);
+            var n = int.Parse(Console.ReadLine());
+
+            while(queue.Count>1)
+            {
+                for (int i = 1; i < n; i++)
+                {
+                    var curr = queue.Dequeue();
+                    queue.Enqueue(curr);
+                }
+
+                var removed = queue.Dequeue();
+                Console.WriteLine($"Removed {removed}");
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
