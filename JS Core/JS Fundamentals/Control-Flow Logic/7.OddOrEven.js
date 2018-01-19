d0cba50afd4affba227798fb71cf5b815d3a97e1
Peter Asenov <peter12345@abv.bg>
2018-01-19 14:37:39 +0200

    Create 7.OddOrEven.js

---
+++ JS Core/JS Fundamentals/Control-Flow Logic/7.OddOrEven.js
@@ -0,0 +1,11 @@
+function evenOddOrInvalid(number) {
+    if(number%2==0) {
+        console.log("even");
+    }
+    else if(number === Math.round(number)) {
+        console.log("odd");
+    }
+    else {
+        console.log("invalid");
+    }
+}
