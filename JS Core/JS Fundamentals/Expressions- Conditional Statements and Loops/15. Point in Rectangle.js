9ab9c11ba8fa32ddb9b1e621ab997c1fe46c4cf0
Peter Asenov <peter12345@abv.bg>
2018-01-23 00:26:35 +0200

    Create 15. Point in Rectangle.js

---
+++ JS Core/JS Fundamentals/Expressions- Conditional Statements and Loops/15. Point in Rectangle.js
@@ -0,0 +1,10 @@
+function pointInRect(input) {
+    let [x,y,xMin,xMax,yMin,yMax] = input;
+
+    if(x>=xMin && x<=xMax && y>=yMin && y<=yMax){
+        return 'inside';
+    }
+    else {
+        return 'outside';
+    }
+}
