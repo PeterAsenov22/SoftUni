22aca0445f7e41f9f87f0fd8d52e15cfc3400a42
Peter Asenov <peter12345@abv.bg>
2018-01-26 17:23:38 +0200

    Create 03. Template Format.js

---
+++ JS Core/JS Fundamentals/Functions and Arrow Functions - Exercise/03. Template Format.js
@@ -0,0 +1,20 @@
+function templateFormat(input) {
+    let result = '<?xml version="1.0" encoding="UTF-8"?>\n';
+    result += '<quiz>\n';
+
+    for (let i = 0; i < input.length; i+=2) {
+        let question = input[i];
+        let answer = input[i+1];
+
+        result +='  <question>\n';
+        result +=`    ${question}\n`;
+        result +='  </question>\n';
+        result +='  <answer>\n';
+        result +=`    ${answer}\n`;
+        result +='  </answer>\n';
+    }
+
+    result += '</quiz>';
+
+    return result;
+}
